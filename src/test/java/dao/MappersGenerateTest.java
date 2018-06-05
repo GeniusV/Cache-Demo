043bbee074695835619a2837207c33f1755c9c66
GeniusV <eliot310100@gmail.com>
2018-06-05 20:02:26 +0800

    add Admin let static config new database

--- src/test/java/dao/MappersGenerateTest.java
+++ src/test/java/dao/MappersGenerateTest.java
@@ -24,8 +24,7 @@ public class MappersGenerateTest {
 
     @Before
     public void before() {
-        //¶ÁÈ¡mybatis²ÎÊý
-        configFile = new File("/Users/GeniusV/Documents/Java/IdeaProjects/Cache-Demo/src/main/resources/generatorConfig.xml");
+        configFile = new File("/Users/GeniusV/Documents/Java/IdeaProjects/collageProjects/Jcimas/src/main/resources/generatorConfig.xml");
 
 //        configFile = new File("/Users/zhangsiyuan/Documents/MybatisFun/Mybatis-Chapter9-GeneratorPlugin/src/main/resources/mybatisConfig.xml");
     }
