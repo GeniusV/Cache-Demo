124b3e0556bf01d42a12b95711301de88d912a09
GeniusV <eliot310100@gmail.com>
2018-06-06 13:32:51 +0800

    redirect to login when not login

--- src/main/java/io/github/geniusv/controller/MainController.java
+++ src/main/java/io/github/geniusv/controller/MainController.java
@@ -1,8 +1,15 @@
 package io.github.geniusv.controller;
 
+import org.apache.shiro.authz.UnauthenticatedException;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by GeniusV on 8/5/17.
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
+    @RequiresAuthentication
     @RequestMapping(value = "home", method = RequestMethod.GET)
     public String getHome() {
         return "/test-home";
@@ -20,5 +28,11 @@ public class MainController {
     @RequestMapping(value = "404")
     public String requestNoFound() {
         return "/404";
+    }
+
+
+    @ExceptionHandler(value = {UnauthenticatedException.class, UnauthorizedException.class})
+    public String exceptionHandler(HttpServletRequest request) {
+        return "redirect:/login";
     }
 }
