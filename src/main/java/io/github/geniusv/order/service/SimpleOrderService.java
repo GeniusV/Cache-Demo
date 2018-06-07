f45b8aaed6c0b308ace7aa567e08cf6a203c8e4e
GeniusV <eliot310100@gmail.com>
2018-06-07 16:15:43 +0800

    order test done

---
+++ src/main/java/io/github/geniusv/order/service/SimpleOrderService.java
@@ -0,0 +1,81 @@
+package io.github.geniusv.order.service;
+
+import io.github.geniusv.dao.mapper.GoodMapper;
+import io.github.geniusv.dao.mapper.OrderMapper;
+import io.github.geniusv.dao.mapper.UserMapper;
+import io.github.geniusv.dao.model.Order;
+import io.github.geniusv.dao.model.OrderExample;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sun.jvm.hotspot.debugger.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by GeniusV on 6/7/18.
+ */
+
+@Service
+public class SimpleOrderService implements OrderService {
+    private Logger logger = LoggerFactory.getLogger("OrderService");
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private GoodMapper goodMapper;
+
+    @Override
+    public boolean createOrder(Long userId, Long goodId) {
+        if (userMapper.selectByPrimaryKey(userId) == null) {
+            logger.warn("UserId: \" " + userId + "\" not exist!!");
+            return false;
+        }
+
+        if (goodMapper.selectByPrimaryKey(goodId) == null) {
+            logger.warn("UserId: \" " + goodId + "\" not exist!!");
+            return false;
+        }
+
+        Order order = new Order();
+        order.setGood(goodId);
+        order.setUserId(userId);
+        order.setNum(1);
+        return orderMapper.insert(order) > 0;
+    }
+
+    @Override
+    public List<Order> selectUserOrder(Long userId, Long page) {
+        OrderExample orderExample = new OrderExample();
+        orderExample.or().andUserIdEqualTo(userId);
+        List<Long> idList = orderMapper.selectPrimaryKeyLimitedByExample(page * 10, 10L, orderExample);
+        List<Order> result = new ArrayList<>();
+        for (Long id : idList) {
+            result.add(orderMapper.selectByPrimaryKey(id));
+        }
+        return result;
+    }
+
+
+    public UserMapper getUserMapper() {
+        return userMapper;
+    }
+
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    public OrderMapper getOrderMapper() {
+        return orderMapper;
+    }
+
+    public void setOrderMapper(OrderMapper orderMapper) {
+        this.orderMapper = orderMapper;
+    }
+}
